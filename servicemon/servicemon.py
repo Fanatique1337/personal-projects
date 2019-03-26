fbb72a13d8df06165da19b916e193df6eb965850
Fanatique1337 <pnikolow1337@gmail.com>
2019-03-26 17:06:04 +0200

    Starting development of servicemon.py

---
+++ servicemon/servicemon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 :
+
+import argparse
+import datetime
+import json
+import os
+import subprocess
+import sys
+import time
+
+# Third party imports:
+
+import psutil
