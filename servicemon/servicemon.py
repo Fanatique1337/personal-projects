dc92894dda68ed301804f753bb2e70ab6168eab1
Fanatique1337 <pnikolow1337@gmail.com>
2019-04-19 19:48:51 +0300

    added tues fest validation script

--- servicemon/servicemon.py
+++ servicemon/servicemon.py
@@ -12,3 +12,46 @@ import time
 # Third party imports:
 
 import psutil
+
+# CONSTANTS:
+
+CONFIG = '/etc/fanatique/servicemon.json'
+DEBUG_TRACE = True
+MEMORY_TYPES_BYTES = ['vms', 'rss', 'swap'] # Types of memory to be monitored.
+MEMORY_TYPES_PERCENT = ['rss', 'swap']
+ATOP_LOGFILE = '/tmp/servicemon.tmp' # Temporary file to save the atop history to.
+ATOP_SAMPLES = 2 # Number of samples to get from atop history.
+
+# Code:
+
+def get_args():
+    """Get arguments and set configuration"""
+
+    try:
+        parser = argparse.ArgumentParser(description="systemd: Service monitoring script")
+        parser.add_argument("-c",
+                            "--config",
+                            help="Configuration file of the script.",
+                            default=CONFIG,
+                            type=str)
+        return parser.parse_args()
+
+    except argparse.ArgumentError:
+        print("Error: Internal argument parsing error occured.", file=sys.stderr)
+        sys.exit(1)
+
+def setup():
+    time_now   = datetime.datetime.now()
+    time_delta = time_now - datetime.timedelta(minutes=ATOP_SAMPLES-1)
+    time_begin = '{}:{}'.format(time_delta.hour, time_delta.minute)
+    time_end   = '{}:{}'.format(time_now.hour, time_now.minute)
+
+    exit_code = subprocess.call("/usr/bin/atop -P PRC,PRM,PRD -b {} -e {} -r > {}".format(
+        time_begin,
+        time_end,
+        ATOP_LOGFILE))
+    
+
+def main():
+    arguments = get_args()
+    
---
+++ tues-fest-validation.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+RESULTS_CSV     = 'tues-fest.tsv'
+VALID_KEYS_FILE = 'valid-keys.txt'
+KEY_INDEX       = 1
+
+def main():
+    with open(RESULTS_CSV, 'r') as file:
+        results = file.readlines()
+
+    with open(VALID_KEYS_FILE) as key_file:
+        valid_keys = key_file.readlines()
+
+    valid_keys = [key.strip() for key in valid_keys]
+
+    # print(valid_keys)
+
+    teams = {}
+    for line in results[1:]:
+        line = line.split('\t')
+        # print(line[KEY_INDEX])
+        # print(line)
+        if check_key_if_valid(valid_keys, line[KEY_INDEX]):
+            for team in line[2:]:
+                team = team.strip()
+                if team in teams:
+                    teams[team] += 1
+                else:
+                    teams[team] = 1
+
+    for key, value in teams.items():
+        print("{}: {}".format(value, key))
+
+def check_key_if_valid(valid_keys, key):
+    if key in valid_keys:
+        del valid_keys[valid_keys.index(key)]
+        return True
+    else:
+        return False
+
+main()
